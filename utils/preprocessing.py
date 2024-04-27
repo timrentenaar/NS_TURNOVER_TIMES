436753b519fd2f5c3e4e0105665091543699eaed
Tim Rentenaar <tim.rentenaar@hva.nl>
2024-04-27 17:45:10 +0200

    initial commit

---
+++ utils/preprocessing.py
@@ -0,0 +1,63 @@
+import pandas as pd
+import numpy as np
+
+def set_types(df):
+    df["DRIVER_CHANGE"] = df["DRIVER_CHANGE"].astype(str)
+    df['TRAINNUMBER'] = df['TRAINNUMBER'].astype(str)
+    df['PREVIOUS_TRAINNUMBER'] = df['PREVIOUS_TRAINNUMBER'].astype(str)
+    df["PLAN_DATETIME"] = pd.to_datetime(df['PLAN_DATETIME'])
+    df["REALIZED_DATETIME"] = pd.to_datetime(df['REALIZED_DATETIME'])
+    df["DEPARTURE_SIGNAL_SHOWS_SAFE"] = pd.to_datetime(df['DEPARTURE_SIGNAL_SHOWS_SAFE'])
+
+    return df
+
+def calc_turnover_end(df):
+    # Create dataframe with rows where there is a turnover and previous trains are valid
+    df_turnover = df.loc[(df["TURNOVER_INDICATOR"] == 1)]
+    valid_trainnumber = set(df['TRAINNUMBER'])  # Valid train numbers
+    filtered_df = df_turnover[df_turnover['PREVIOUS_TRAINNUMBER'].isin(valid_trainnumber)]
+
+    # Filter DataFrame to include only arrival records
+    arrival_df = df[df['ACTIVITYTYPE'].isin(['A', 'K_A'])]
+    plan_latest_arrivals = arrival_df.groupby(['TRAFFIC_DATE', 'TRAINNUMBER', 'STATION'])['PLAN_DATETIME'].max()
+    real_latest_arrivals = arrival_df.groupby(['TRAFFIC_DATE', 'TRAINNUMBER', 'STATION'])['REALIZED_DATETIME'].max()
+
+    # Calc planned turnover time per row
+    plan_turnover_time = filtered_df.apply(
+        lambda row: row['PLAN_DATETIME'] - plan_latest_arrivals.get((row['TRAFFIC_DATE'], row['PREVIOUS_TRAINNUMBER'], row['STATION']), pd.NaT),
+        axis=1
+    ).dt.total_seconds()
+
+    # Calc realized turnover time per row
+    real_turnover_time = filtered_df.apply(
+        lambda row: row['REALIZED_DATETIME'] - real_latest_arrivals.get((row['TRAFFIC_DATE'], row['PREVIOUS_TRAINNUMBER'], row['STATION']), pd.NaT),
+        axis=1
+    ).dt.total_seconds()
+
+    # Set new turnover time columns
+    filtered_df["PLAN_TURNOVER_TIME"] = plan_turnover_time
+    filtered_df["REALIZED_TURNOVER_TIME"] = real_turnover_time
+
+    return filtered_df
+
+def calc_turnover_middle(df):
+    # Arrivals of previous rows
+    plan_previous_row_arrival = df['PLAN_DATETIME'].shift(1)
+    real_previous_row_arrival = df['REALIZED_DATETIME'].shift(1)
+
+    # Create dataframe with rows where there is a turnover and no previous trains are indicated
+    df_turnover = df.loc[(df["TURNOVER_INDICATOR"] == 1)]
+    df_nan = df_turnover[df_turnover['PREVIOUS_TRAINNUMBER'] == 'nan']
+
+    # Calculate turnover times
+    plan_turnover_time = (df_nan['PLAN_DATETIME'] - plan_previous_row_arrival.loc[df_nan.index]).dt.total_seconds()
+    real_turnover_time = (df_nan['REALIZED_DATETIME'] - real_previous_row_arrival.loc[df_nan.index]).dt.total_seconds()
+    
+    # Set new turnover time columns
+    df_nan["PLAN_TURNOVER_TIME"] = plan_turnover_time
+    df_nan["REALIZED_TURNOVER_TIME"] = real_turnover_time
+
+    return df_nan
+
+def calc_turnover(df):
+    return pd.concat([calc_turnover_middle(df), calc_turnover_end(df)])
